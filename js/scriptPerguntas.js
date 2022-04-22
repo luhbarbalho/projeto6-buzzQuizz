4a78a02f0af5add0dbb40397130a73762fa4f667
luhbarbalho <luh.barbalho@gmail.com>
2022-04-22 17:07:35 -0300

    novas alterações

--- js/scriptPerguntas.js
+++ js/scriptPerguntas.js
@@ -1,5 +1,9 @@
 let basicoCerto;
 let perguntaCerto;
+let nivelCerto;
+let quantPerguntas;
+let quantNiveis;
+
 
 
 function conferenciaBasica() {
@@ -7,8 +11,8 @@ function conferenciaBasica() {
 
     const tituloQuizz = document.querySelector(".tituloQuizz").value;
     const urlQuizz = document.querySelector(".urlQuizz").value;
-    const quantPerguntas = document.querySelector(".quantPerguntas").value;
-    const quantNiveis = document.querySelector(".quantNiveis").value;
+    quantPerguntas = document.querySelector(".quantPerguntas").value;
+    quantNiveis = document.querySelector(".quantNiveis").value;
 
 
     if ((tituloQuizz.length >= 20) && (tituloQuizz.length <= 65)){
@@ -29,37 +33,92 @@ function conferenciaBasica() {
 }
 
 
+function gerarCriePerguntas() {
+    
+}
+
+
 
 function conferenciaPergunta() {
 
     perguntaCerto = 0
 
     const perguntaInput = document.querySelector(".perguntaInput").value;
-    const PerguntaCor = document.querySelector(".PerguntaCor").value;
-    const RespostaCorreta = document.querySelector(".RespostaCorreta").value;
-    const URLRespostaCorreta = document.querySelector(".URLRespostaCorreta").value;
+    const perguntaCor = document.querySelector(".perguntaCor").value;
+    const respostaCorreta = document.querySelector(".respostaCorreta").value;
+    const URLrespostaCorreta = document.querySelector(".URLrespostaCorreta").value;
 
-    const RespostaIncorreta1 = document.querySelector(".RespostaIncorreta1").value;
-    const URLRespostaIncorreta1 = document.querySelector(".URLRespostaIncorreta1").value;
-    const RespostaIncorreta2 = document.querySelector(".RespostaIncorreta2").value;
-    const URLRespostaIncorreta2 = document.querySelector(".URLRespostaIncorreta2").value;
-    const RespostaIncorreta3 = document.querySelector(".RespostaIncorreta3").value;
-    const URLRespostaIncorreta3 = document.querySelector(".URLRespostaIncorreta3").value;
+    const respostaIncorreta1 = document.querySelector(".respostaIncorreta1").value;
+    const URLrespostaIncorreta1 = document.querySelector(".URLrespostaIncorreta1").value;
+    const respostaIncorreta2 = document.querySelector(".respostaIncorreta2").value;
+    const URLrespostaIncorreta2 = document.querySelector(".URLrespostaIncorreta2").value;
+    const respostaIncorreta3 = document.querySelector(".respostaIncorreta3").value;
+    const URLrespostaIncorreta3 = document.querySelector(".URLrespostaIncorreta3").value;
 
 
-    if ((perguntaInput.length >= 20)){
+    if ((perguntaInput.length >= 20) && (typeof perguntaInput == 'string')){
+        perguntaCerto += 2;
+    } if (typeof perguntaCor == 'string') { // AJEITAR O CÓDIGO PARA TESTAR #hexacdecimal.
+        perguntaCerto += 2;
+    } if ((typeof respostaCorreta == 'string')&&(respostaCorreta !== " ")) {
+        perguntaCerto += 2;
+    } if (typeof URLrespostaCorreta == 'string') {// AJEITAR O CÓDIGO PARA TESTAR URL.
+        perguntaCerto += 2;
+    } if ((typeof respostaIncorreta1 == 'string')&&(respostaIncorreta1 !== " ")) {
+        perguntaCerto += 2;
+    } if (typeof URLrespostaIncorreta1 == 'string') {// AJEITAR O CÓDIGO PARA TESTAR URL.
+        perguntaCerto += 2;
+    } if ((typeof respostaIncorreta2 == 'string')&&(respostaIncorreta2 !== " ")) {
         perguntaCerto += 1;
-    } if (PerguntaCor == 'string') { // AJEITAR O CÓDIGO PARA TESTAR URL.
+    } if (typeof URLrespostaIncorreta2 == 'string') {// AJEITAR O CÓDIGO PARA TESTAR URL.
         perguntaCerto += 1;
-    } if (parseInt(quantPerguntas) >= 3) {
+    } if ((typeof respostaIncorreta3 == 'string')&&(respostaIncorreta3 !== " ")) {
         perguntaCerto += 1;
-    } if (parseInt(quantNiveis) >=2) {
+    } if (typeof URLrespostaIncorreta3 == 'string') {// AJEITAR O CÓDIGO PARA TESTAR URL.
         perguntaCerto += 1;
     }
-    if (perguntaCerto == 4) {
-        document.querySelector(".comeceComeco").classList.add("displayNone");
-        document.querySelector(".criePerguntas").classList.remove("displayNone");
-    } if (perguntaCerto < 4) {
+
+    
+
+    if (perguntaCerto >= 12) {
+        document.querySelector(".criePerguntas").classList.add("displayNone");
+        document.querySelector(".decidaNiveis").classList.remove("displayNone");
+    } if (perguntaCerto < 12) {
         alert("Tem alguma informação errada aí!");
     }
+}
+
+
+
+
+
+
+function conferenciaNiveis() {
+    nivelCerto = 0;
+
+    const tituloNivel = document.querySelector(".tituloNivel").value;
+    const acerto = document.querySelector(".acerto").value;
+    const urlimg = document.querySelector(".urlimg").value;
+    const descricaoNivel = document.querySelector(".descricaoNivel").value;
+
+
+    if ((tituloNivel.length >= 10) && (typeof tituloNivel == 'string')){
+        nivelCerto += 1;
+    } if ((parseInt(acerto) >= 0) && (parseInt(acerto) <= 100)) {
+        nivelCerto += 1;
+    } if (typeof urlimg == 'string') {// AJEITAR O CÓDIGO PARA TESTAR URL.
+        nivelCerto += 1;
+    } if ((descricaoNivel.length >= 30) && (typeof descricaoNivel == 'string')) {
+        nivelCerto += 1;
+    }
+
+
+    if (nivelCerto == 4) {
+        document.querySelector(".decidaNiveis").classList.add("displayNone");
+        document.querySelector(".quizzPronto").classList.remove("displayNone");
+    } if (nivelCerto < 4) {
+        alert("Tem alguma informação errada aí!");
+    }
+
+
 }
